ceb064246abc75c0ecaaa34169134743ca85d2b5
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2022-10-28 22:20:57 +0200

    Create extra safety-steps after importing template data

--- script_batch_callculation.R
+++ script_batch_callculation.R
@@ -1,26 +1,50 @@
 library(tidyverse)
 library(openxlsx)
 
-# Load the data 
-# Alkanes 
-alkanes <- read.xlsx("KI_upload_template.xlsx", sheet = 1) %>% 
+# Load the data and clean up 
+
+###################
+# Alkane standard #
+###################
+
+alkanes <- read.xlsx("KI_upload_template.xlsx", sheet = 1) %>%
+  select(1:3) # Only first 3 columns if people scew with the template and extra columns are added
+
+# Rename colnames, in case people screw up the template
+colnames(alkanes) <- c("name", "carbons", "RT_seconds") 
+
+# Filter rows with missing information
+alkanes <- alkanes %>% 
   filter(!is.na(carbons) & !is.na(RT_seconds)) # name of the alkane can be empty - its not used in the script
 
-# Compounds of interest
-compounds_of_interest <- read.xlsx("KI_upload_template.xlsx", sheet = 2) %>% 
-  filter(!is.na(compounds_of_interest) & !is.na(RT_seconds))
 
+#########################
+# Compounds of interest #
+#########################
 
-# Plot retention time progression of the alkane series
+compounds_of_interest <- read.xlsx("KI_upload_template.xlsx", sheet = 2) %>%
+  select(1:2) # Only first 2 columns if people scew with the template and columns are added
+
+# Rename colnames, in case people screw up the template
+colnames(compounds_of_interest) <- c("compound_id", "RT_seconds") 
+
+# Filter rows with missing info to calculate 
+compounds_of_interest <- compounds_of_interest %>%
+  filter(!is.na(compound_id) & !is.na(RT_seconds))
+
+################################
+# Plot RT of the alkane series #
+################################
+
 ggplot(alkanes,aes(x = carbons,  y = RT_seconds))+
   geom_point()+
   geom_line()
 
+##############################################
+# Function to calculate the KI of a comound  #
+##############################################
 
-###########################################################
-# Set the N-1 / N+1 points from your compound of interest #
-###########################################################
-
+# my.rt = Retention time of your compound of interest
 # RT1 = Retention time of the first alkane before "my.rt" 
 # RT2 = Retention time of the first alkane after "my.rt" 
 # N = The number of carbons of the alkane that comes after "my.rt"
