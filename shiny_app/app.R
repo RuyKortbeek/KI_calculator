bac96a251557d5d6a8bac62907c0319221c125a6
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2022-11-03 20:47:26 +0100

    Make input data reactive

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -26,10 +26,17 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  alkanes <- reactive({
+    alkanes <- read.xlsx(input$datafile$datapath, sheet = 1)   
+  })
+  
+  metabolites_of_interest <- reactive({
+    metabolites_of_interest <- read.xlsx(input$datafile$datapath, sheet = 2)   
+  })
   # plot alkane series from upload
   output$alkanes_plot <- 
     renderPlot(
-    read.xlsx(input$datafile$datapath, sheet = 1) %>% 
+      read.xlsx(input$datafile$datapath, sheet = 1) %>% 
       ggplot(aes(x = carbons, y = RT_seconds)) + 
       geom_line()+
       geom_point()
@@ -40,9 +47,9 @@ server <- function(input, output) {
   ############################
   
   fun.KI <- function(my.rt){ 
-    RT1 = read.xlsx(input$datafile$datapath, sheet = 1)  %>% filter(RT_seconds < my.rt) %>% tail(1) %>% .$RT_seconds
-    RT2= read.xlsx(input$datafile$datapath, sheet = 1)  %>% filter(RT_seconds > my.rt) %>% head(1) %>% .$RT_seconds
-    N = read.xlsx(input$datafile$datapath, sheet = 1)  %>% filter(RT_seconds > my.rt) %>% head(1) %>% .$carbons
+    RT1 = alkanes() %>% filter(RT_seconds < my.rt) %>% tail(1) %>% .$RT_seconds
+    RT2= alkanes() %>% filter(RT_seconds > my.rt) %>% head(1) %>% .$RT_seconds
+    N = alkanes()  %>% filter(RT_seconds > my.rt) %>% head(1) %>% .$carbons
     
     KI = round((100 * N)+100*((my.rt - RT1)/(RT2 - RT1)))
     
@@ -51,7 +58,7 @@ server <- function(input, output) {
   
   output$compounds_with_KI <-
     renderTable(
-      read.xlsx(input$datafile$datapath, sheet = 2) %>% 
+      metabolites_of_interest() %>% 
         mutate(calculated_KI = lapply(RT_seconds, fun.KI))
     )
     
