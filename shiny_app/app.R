dedc13f39bdac239ed359f1464d07abcdb77578a
Ruy Kortbeek <R.W.J.Kortbeek@uva.nl>
2022-11-03 19:52:18 +0100

    add upload button

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -7,9 +7,12 @@ library(openxlsx)
 ###################
 
 ui <- fluidPage(
-  titlePanel("KI Calculator"),
+  titlePanel("Kovats Index Calculator"),
   sidebarLayout(
-    sidebarPanel("our inputs will go here"),
+    sidebarPanel(
+      fileInput(inputId = "data.file", label = "upload XLS file", accept = ".xlsx")
+      
+    ),
     mainPanel("the results will go here")
   )
 )
